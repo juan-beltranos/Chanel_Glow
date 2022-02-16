04cc6b55b4169ee406eab276961d991bc8487f0d
Sebastian Beltran <sebastianbeltran341@gmail.com>
2022-02-16 16:08:10 -0500

    CRUD Servicios

---
+++ controllers/serviciosControllers.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Controllers;
+
+use Model\Servicio;
+
+class serviciosControllers
+{
+    public static function getServicios()
+    {
+        $servicios = Servicio::all();
+        echo json_encode($servicios);
+    }
+
+    public static function getServiciosId()
+    {
+        $servicio = Servicio::where('id', $_GET['id']);
+        if (!$servicio) {
+            $respuesta = [
+                'tipo' => 'error',
+                'mensaje' => 'No existe ese servicio'
+            ];
+            echo json_encode($respuesta);
+            return;
+        }
+        echo json_encode($servicio);
+    }
+
+    public static function postServicio()
+    {
+        $servicio = new Servicio;
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $servicio->sincronizar($_POST);
+
+
+            if (!$servicio->nombre || !$servicio->precio) {
+                $res = [
+                    'tipo' => 'error',
+                    'msg' => 'Ningun campo puede ir vacio'
+                ];
+                echo json_encode($res);
+                return;
+            }
+
+            $servicio->crear();
+            $respuesta = [
+                'tipo' => 'exito',
+                'mensaje' => 'Servicio Creado Correctamente',
+            ];
+            echo json_encode($respuesta);
+        }
+    }
+
+    public static function putServicio()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Validar que el servicio exista
+            $servicio = Servicio::where('id', $_GET['id']);
+
+            if (!$servicio) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un Error al actualizar el servicio'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+
+            $servicioPut = new Servicio($_POST);
+            $servicioPut->id = $servicio->id;
+            $resultado = $servicioPut->actualizar();
+
+            if ($resultado) {
+                $respuesta = [
+                    'tipo' => 'exito',
+                    'id' => $servicioPut->id,
+                    'mensaje' => 'Actualizado correctamente'
+                ];
+                echo json_encode(['respuesta' => $respuesta]);
+            }
+        }
+    }
+
+    public static function deleteServicio()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Validar que el servicio exista
+            $servicio = Servicio::where('id', $_GET['id']);
+
+            if (!$servicio) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un Error al actualizar la tarea'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+
+            $servicioDelete = new Servicio($_POST);
+            $servicioDelete->id = $servicio->id;
+
+            $resultado = $servicioDelete->eliminar();
+
+
+            $resultado = [
+                'resultado' => $resultado,
+                'mensaje' => 'Eliminado Correctamente',
+                'tipo' => 'exito'
+            ];
+
+            echo json_encode($resultado);
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,15 +2,18 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
-use Controllers\citasControllers;
+use Controllers\serviciosControllers;
 use MVC\Router;
 
 $router = new Router();
 
 
 // Servicios
-$router->get('/api/servicios', [citasControllers::class, 'getServicios']);
-$router->post('/api/servicios', [citasControllers::class, 'postServicio']);
+$router->get('/api/servicios', [serviciosControllers::class, 'getServicios']);
+$router->get('/api/serviciosId', [serviciosControllers::class, 'getServiciosId']);
+$router->post('/api/servicios', [serviciosControllers::class, 'postServicio']);
+$router->post('/api/servicios/actualizar', [serviciosControllers::class, 'putServicio']);
+$router->post('/api/servicios/eliminar', [serviciosControllers::class, 'deleteServicio']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
